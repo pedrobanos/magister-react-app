a634e8f092bb6d4e774cf670701af5f949cd240e
Pedro Baños <pedrobanos.renault@gmail.com>
2022-07-16 12:35:23 +0200

    created project and linked to Firebase done

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3pcDnsEUc_ajhnwoGhBZe652XSkKnruI",
+  authDomain: "magister-react-app.firebaseapp.com",
+  projectId: "magister-react-app",
+  storageBucket: "magister-react-app.appspot.com",
+  messagingSenderId: "1020404754369",
+  appId: "1:1020404754369:web:da839e0116980e2dfab140"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+
+    return(
+        <di>
+            HERE MUST STAY HOME 
+        </di>
+    )
+}
+
+export default Home 
